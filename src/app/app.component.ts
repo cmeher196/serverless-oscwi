58ee4749ea8059643e386afdcf83d75adf187313
chamehe1 <chamehe1@in.ibm.com>
2021-09-28 23:51:54 +0530

    initial setup for the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServerlessService } from './serverless.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'serverless-oscwi';
+
+  public formData = {
+    name: '',
+    id: '',
+    email: '',
+    number: ''
+  }
+
+
+  constructor(private service: ServerlessService){}
+
+  ngOnInit() {
+
+  }
+
+  handleFormSubmit = (event: any) => {
+    // event.preventDefault();
+    console.log("form submitted", this.formData);
+
+    this.service.saveuser(this.formData).subscribe(data=>console.log(data)
+    )
+
+  }
+
+
+
 }
---
+++ src/app/serverless.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServerlessService {
+
+  constructor(private http: HttpClient) { }
+
+  public url : string = 'https://470c5664.eu-gb.apigw.appdomain.cloud/oscwi/entries';
+
+  saveuser (user:any) :Observable<any>{
+    return this.http.put(this.url, user)
+  }
+}
